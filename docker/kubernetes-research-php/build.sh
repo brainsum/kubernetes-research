df00547cdccbc23ea2d4a2e636bf52a051f818e0
mhavelant <mhavelant@brainsum.com>
2020-07-09 13:06:13 +0200

    Buildkit, misc fixes.

--- docker/kubernetes-research-php/build.sh
+++ docker/kubernetes-research-php/build.sh
@@ -8,5 +8,12 @@ IMAGE_TAG=$(cat "${SCRIPT_DIR}/version.txt");
 IMAGE="${IMAGE_NAME}:${IMAGE_TAG}"
 
 echo "Building tag: ${IMAGE}"
+CONTEXT="${SCRIPT_DIR}/../.."
 
-docker build --rm -f "${SCRIPT_DIR}/Dockerfile" -t "${IMAGE}" --build-arg BASE_IMAGE_TAG="${IMAGE_TAG}" "${SCRIPT_DIR}/../.."
+# shellcheck disable=SC2034
+DOCKER_BUILDKIT=1 docker build \
+    --rm \
+    -f "${SCRIPT_DIR}/Dockerfile" \
+    -t "${IMAGE}" \
+    --build-arg BASE_IMAGE_TAG="${IMAGE_TAG}" \
+    "${CONTEXT}"
