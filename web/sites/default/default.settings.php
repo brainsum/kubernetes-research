ae6aff033ccc297ca7c26d16ebb0db52c5467da0
mhavelant <mhavelant@brainsum.com>
2020-07-09 14:12:45 +0200

    Cleanup.

--- web/sites/default/default.settings.php
+++ web/sites/default/default.settings.php
@@ -755,7 +755,3 @@ $settings['entity_update_backup'] = TRUE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-
-if (file_exists(__DIR__ . '/../damo.settings.php')) {
-  include_once __DIR__ . '/../damo.settings.php';
-}
