2a0fe3072d3cd766cf241a3eaca96d192b8eae3c
mhavelant <mhavelant@brainsum.com>
2020-07-28 13:56:45 +0200

    Add a deploy Job for drupal, update the deploy script for this.

--- kubernetes/drupal-deploy.sh
+++ kubernetes/drupal-deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-POD_NAME=$(kubectl -n demok8s get pods --selector='app=demok8s-app' -o jsonpath='{.items[:1].metadata.name}')
-kubectl -n demok8s exec -it "${POD_NAME}" -c drupal -- bash -c "drush updb -y && drush cim -y && drush cr && drush cron"
+kubectl -n demok8s apply -f drupal-deployment-job.yaml
