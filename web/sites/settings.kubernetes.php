5ed329c55c5293bae331b68722970c131834947d
mhavelant <mhavelant@brainsum.com>
2020-08-04 16:55:29 +0200

    Fix k8s settings.php.

--- web/sites/settings.kubernetes.php
+++ web/sites/settings.kubernetes.php
@@ -13,7 +13,11 @@ if (file_exists(__DIR__ . '/settings.solr.php')) {
   include_once __DIR__ . '/settings.solr.php';
 }
 
-$config['backend_config']['connector'] = 'solr_cloud';
+# @todo: Basic auth.
+//$config['search_api.server.solr']['backend_config']['connector'] = 'solr_cloud_basic_auth';
+//$config['search_api.server.solr']['backend_config']['connector_config']['username'] = getenv('SOLR_USERNAME');
+//$config['search_api.server.solr']['backend_config']['connector_config']['password'] = getenv('SOLR_PASSWORD');
+$config['search_api.server.solr']['backend_config']['connector'] = 'solr_cloud';
 $config['search_api.server.solr']['backend_config']['connector_config']['checkpoints_collection'] = '';
 $config['search_api.server.solr']['backend_config']['connector_config']['stats_cache'] = 'org.apache.solr.search.stats.LRUStatsCache';
 
